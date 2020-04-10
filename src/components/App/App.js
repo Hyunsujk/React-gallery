2e7311ab807547d9dec4f67b284c36aebc742024
Hyunsujk <hyunsujk@gmail.com>
2020-04-10 17:22:44 -0500

    added getGallery to get list of gallery. test good

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,42 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
 
 class App extends Component {
+  state = {
+    galleryItems: [],
+  };
+
+  componentDidMount() {
+    console.log("in mount");
+    this.getGallery();
+  }
+  //
+  // API SERVER CALLS
+  //----------------
+
+  getGallery() {
+    axios({
+      method: "GET",
+      url: "/gallery",
+    })
+      .then((response) => {
+        console.log("server response", response.data);
+      })
+      .catch((err) => {
+        console.log("Error:", err);
+      });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Gallery of my life</h1>
         </header>
-        <br/>
+        <br />
         <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        <img src="images/goat_small.jpg" />
       </div>
     );
   }
