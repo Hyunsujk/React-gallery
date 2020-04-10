a77efd0530769acdae9ca9edc2a996af3f4ad846
Hyunsu Kim <hyunsu.j.kim@gmail.com>
2020-04-10 18:08:04 -0500

    componentized GalleryItem to utilize it in GalleryList. test good.

--- src/components/GalleryItem/GelleryItem.js
+++ src/components/GalleryItem/GelleryItem.js
@@ -1,1 +1,16 @@
 import React, { Component } from "react";
+
+class GalleryItem extends Component {
+  render() {
+    const gallery = this.props.gallery.map((gallery, index) => {
+      return (
+        <div key={index}>
+          <img src={gallery.path} alt="animal" />
+        </div>
+      );
+    });
+    return <div>{gallery}</div>;
+  }
+}
+
+export default GalleryItem;
--- src/components/GalleryList/GalleryList.js
+++ src/components/GalleryList/GalleryList.js
@@ -1,15 +1,13 @@
 import React, { Component } from "react";
+import GalleryItem from "../GalleryItem/GelleryItem";
 
 class GalleryList extends Component {
   render() {
-    const displayGallery = this.props.gallery.map((gallery, index) => {
-      return (
-        <div key={index}>
-          <img src={gallery.path} alt="animal" />
-        </div>
-      );
-    });
-    return <div>{displayGallery}</div>;
+    return (
+      <div>
+        <GalleryItem gallery={this.props.gallery} />
+      </div>
+    );
   }
 }
 
