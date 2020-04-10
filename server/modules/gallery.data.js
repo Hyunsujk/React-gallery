4df7a2bfee71d8dfc8603ceddfd8ce4a416853f5
Hyunsujk <hyunsujk@gmail.com>
2020-04-10 16:43:38 -0500

    add more pictures and updated galleryItems

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,22 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+  {
+    id: 1,
+    path: "images/goat_small.jpg",
+    description: "Photo of a goat taken at Glacier National Park.",
+    likes: 0,
+  },
+  {
+    id: 2,
+    path: "../../public/images/cat.jpg",
+    description: "Photo of a grumpy cat.",
+    likes: 0,
+  },
+  {
+    id: 3,
+    path: "../../public/images/dog.jpg",
+    description: "Photo of a cute dog.",
+    likes: 0,
+  },
 ];
 
 module.exports = galleryItems;
