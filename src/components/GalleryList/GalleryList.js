9951c285ec159a3a0a8a5503a3940ab320a7459a
Hyunsu Kim <hyunsu.j.kim@gmail.com>
2020-04-10 19:21:45 -0500

    likes number increase 1 when like button is clicked. test good

--- src/components/GalleryList/GalleryList.js
+++ src/components/GalleryList/GalleryList.js
@@ -4,7 +4,13 @@ import GalleryItem from "../GalleryItem/GelleryItem";
 class GalleryList extends Component {
   render() {
     const galleryList = this.props.gallery.map((gallery, index) => {
-      return <GalleryItem key={index} gallery={gallery} />;
+      return (
+        <GalleryItem
+          key={index}
+          gallery={gallery}
+          updateGalleryLikes={this.props.updateGalleryLikes}
+        />
+      );
     });
     return <div>{galleryList}</div>;
   }
