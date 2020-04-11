bd74a5765dd88ca38f9424f8942718545bc7e80c
Hyunsu Kim <hyunsu.j.kim@gmail.com>
2020-04-11 13:55:54 -0500

    added router.get to interact with database. test good

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const galleryItems = require("../modules/gallery.data");
+const pool = require("../modules/pool");
 
 // DO NOT MODIFY THIS FILE FOR BASE MODE
 
@@ -29,9 +30,36 @@ router.put("/clicked/:id", (req, res) => {
   res.sendStatus(200);
 }); // END PUT Route
 
+// router.put("/:id", (req, res) => {
+//     const itemId = req.params.id;
+//     const completedItem = req.body;
+//     const queryText = `UPDATE "list" SET "item"=$1, "complete"=$2 WHERE "id"=$3;`;
+//     pool
+//       .query(queryText, [completedItem.item, completedItem.complete, itemId])
+//       .then((responseDB) => {
+//         res.sendStatus(200);
+//       })
+//       .catch((err) => {
+//         console.log("Error:", err);
+//         res.sendStatus(500);
+//       });
+//   });
+
 // GET Route
+
 router.get("/", (req, res) => {
-  res.send(galleryItems);
+  const queryText = `SELECT * FROM "gallery" ORDER BY "id" ASC;`;
+  pool
+    .query(queryText)
+    .then((responseDB) => {
+      const dbRows = responseDB.rows;
+      console.table(dbRows);
+      res.send(dbRows);
+    })
+    .catch((err) => {
+      console.log("Error:", err);
+      res.sendStatus(500);
+    });
 }); // END GET Route
 
 module.exports = router;
