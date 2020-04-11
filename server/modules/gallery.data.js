450d7d75753e1cec7d3fe650f58fbc49db3aa645
Hyunsujk <hyunsujk@gmail.com>
2020-04-10 21:25:03 -0500

    added contentClicked to switch content from picture to description. test good

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -4,18 +4,21 @@ const galleryItems = [
     path: "images/goat_small.jpg",
     description: "Photo of a goat taken at Glacier National Park.",
     likes: 0,
+    isClicked: false,
   },
   {
     id: 2,
     path: "images/cat.jpg",
     description: "Photo of a grumpy cat.",
     likes: 0,
+    isClicked: false,
   },
   {
     id: 3,
     path: "images/dog.jpg",
     description: "Photo of a cute dog.",
     likes: 0,
+    isClicked: false,
   },
 ];
 
--- src/components/GalleryItem/GelleryItem.js
+++ src/components/GalleryItem/GelleryItem.js
@@ -2,24 +2,24 @@ import React, { Component } from "react";
 import "./GalleryItem.css";
 
 class GalleryItem extends Component {
-  flipContent = () => {
-    console.log("flipping content");
-  };
   render() {
+    const isClicked = this.props.gallery.isClicked;
+    let content;
+    if (isClicked) {
+      content = this.props.gallery.description;
+    } else {
+      content = <img src={this.props.gallery.path} alt="gallery" />;
+    }
     return (
-      <td>
-        <tr onClick={this.flipContent}>
-          <img src={this.props.gallery.path} alt="picture" />
-        </tr>
-        <tr>
-          <p>Likes:{this.props.gallery.likes}</p>
-          <button
-            onClick={this.props.updateGalleryLikes(this.props.gallery.id)}
-          >
-            Like
-          </button>
-        </tr>
-      </td>
+      <div>
+        <div onClick={this.props.contentClicked(this.props.gallery.id)}>
+          {content}
+        </div>
+        <p>Likes:{this.props.gallery.likes}</p>
+        <button onClick={this.props.updateGalleryLikes(this.props.gallery.id)}>
+          Like
+        </button>
+      </div>
     );
   }
 }
